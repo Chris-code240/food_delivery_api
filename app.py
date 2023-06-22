101e539992551f92200b456fd9fe4c8c89d46ec4
Chris-code240 <duah.marfochristian@gmail.com>
2023-06-22 11:01:04 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,147 @@
+from flask import request,abort,json,jsonify,make_response,redirect
+import jwt
+import datetime
+import os
+from models import app,Resturant,Menu,Order,User,startDb,AuthError
+from functools import wraps
+# startDb()
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args,**kwargs):
+        token = os.environ['TOKEN']
+        if not token:
+            abort(401)
+        try:
+            data = jwt.decode(token,app.secret_key,['HS256'])
+        except Exception as e:
+            print(e)
+            abort(500)
+        return f(*args,**kwargs)
+    return decorated
+
+
+
+
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if auth == None:
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
+    parts = auth.split(' ')
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+    else:
+        token = parts[1]
+        return token
+
+@app.route('/menus')
+def get_all_menus():
+    try:
+        menus = [m.getMenu() for m in Menu.query.all()]
+    except:
+        abort(500)
+    return jsonify({"success":True,"menus":menus})
+
+
+
+
+
+
+@app.route('/order',methods=['POST'])
+@token_required
+def place_order():
+    try:
+        req = request.get_json()
+        order = Order(req['user_id'],req['resturant_id'],req['menu_id'],req['items'])
+        order.addOrder() 
+    except:
+        abort(500)
+    return jsonify({"success":True,"order":order.getOrder()})    
+
+
+@app.route('/complete-order',methods=['PATCH'])
+def complete_order():
+    id = request.get_json()['id']
+    try:
+        order = Order.query.filter(Order.id == id).one_or_none()
+        if order is not None:
+            order.completeOrder()
+            return jsonify({"success":True,"message":f"Order number {id} has been fulfilled"})
+        abort(404)
+    except Exception as e:
+        return jsonify({"message":str(e)})
+
+
+
+
+@app.route('/login')
+def verify():
+    auth = request.authorization
+    if auth:
+        name  = auth.username
+        password = auth.password
+        user = User.query.filter(User.name == name, User.password == password).one_or_none()
+        if user is not None:
+            os.environ['TOKEN'] = jwt.encode({"user":name,"exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.secret_key,"HS256")
+            return  jsonify({"success":True,"token":os.environ.get('TOKEN')})
+    return make_response('Could not verify',401,{'WWW-Authenticate':'Basic real="Login Required'})
+
+"""
+view menus
+place order
+order fulfillment
+"""
+
+
+
+
+@app.errorhandler(400)
+def internal_error(code):
+    return jsonify({"success":False,"message":"Bad Request", 
+    "error": code,}), 400
+@app.errorhandler(500)
+def internal_error(code):
+    return jsonify({"success":False,"message":"Internal Erro", 
+    "error": code,}), 500
+    
+@app.errorhandler(404)
+def not_found(code):
+    return jsonify({"success":False,"message":"Resource not found", 
+    "error": code,}), 404
+
+@app.errorhandler(401)
+def user_not_authorized(code):
+    return jsonify({"success":False,"message":"User is not authorized.", 
+    "error": code,}), 401
+
+
+@app.errorhandler(405)
+def method_not_alloed(code):
+
+    return jsonify({"success":False,"message":f"{request.method} is not allowed on this endpoint", 
+    "error": code,}), 405
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,143 @@
+from flask import Flask,jsonify,json,abort
+from flask_sqlalchemy import SQLAlchemy
+import os
+import flask_cors
+
+app = Flask(__name__)
+app.config.from_object('config')
+app.secret_key = os.environ['SECRETE_KEY']
+flask_cors.CORS(app)
+
+db = SQLAlchemy(app)
+
+def startDb():
+    db.drop_all()
+    db.create_all()
+
+    res = Resturant("Resturant 1")
+    res.addResturant()
+    menu = Menu(1)
+    menu.addMenu()
+
+    for item in ['fufuo','banku','rice ball']:
+        menu.addItem(item)
+
+    user = User('Chris','mypassword')
+    user.addUser()
+
+    order = Order(1,1,1,['fufuo','rice ball'])
+    order.addOrder()
+    # for item in ['rice ball','fufuo']:
+    #     order.addItem(item)
+
+
+class Resturant(db.Model):
+    __tablename__ = "Resturant"
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(),nullable=False)
+
+    def __init__(self,name) -> None:
+        self.name = name
+    
+    def addResturant(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def getResturant(self):
+        try:
+            menus = [m.getMenu() for m in Menu.query.filter(Menu.returant_id == self.id).all()]
+            orders = [order.getOrder() for order in Order.query.filter(Order.resturant_id == self.id).all()]
+        except:
+            abort(500)
+        return {"success":True,"id":self.id,"name":self.name,"menus":menus, "orders":orders}
+
+class Menu(db.Model):
+    __tablename__ = "Menu"
+    id = db.Column(db.Integer,primary_key=True)
+    returant_id = db.Column(db.Integer,db.ForeignKey('Resturant.id'), nullable=False)
+    
+    items = db.Column(db.String())
+
+    def __init__(self,resturant_id) -> None:
+        self.returant_id = resturant_id
+        self.items = "[]"
+        
+    def addMenu(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    def getMenu(self):
+        orders = Order.query.filter(Order.menu_id == self.id).all()
+        return {"id":self.id,"resturant_id":self.returant_id,"items":json.loads(self.items)}
+    
+    def addItem(self,item):
+        items = json.loads(self.items)
+        items.append(item)
+        self.items = json.dumps(items)
+        db.session.commit()
+
+class User(db.Model):
+    __tablename__ = "User"
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(),nullable=False)
+    password = db.Column(db.String(),nullable=False)
+    def __init__(self,name,password) -> None:
+        self.name = name
+        self.password = password
+    
+    def addUser(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def getUser(self):
+        orders = [order.getOrder() for order in Order.query.filter(Order.user_id == self.id).all()]
+        return {"id":self.id,"name":self.name,"orders":orders}
+    
+    def getUser(self):
+        return {"id":self.id,"name":self.name}
+        
+class Order(db.Model):
+    __tablename__ = "Order"
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('User.id'),nullable=False)
+    resturant_id = db.Column(db.Integer(),db.ForeignKey('Resturant.id'))
+    menu_id = db.Column(db.Integer,db.ForeignKey('Order.id'))
+    items = db.Column(db.String(),nullable=False,default="[]")
+    completed = db.Column(db.Boolean,default=False)  
+
+    def __init__(self,user_id,resturant_id,menu_id,items) -> None:
+        self.user_id = user_id
+        self.resturant_id = resturant_id
+        self.menu_id = menu_id
+        self.items = json.dumps(items)
+        
+    def addOrder(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def completeOrder(self):
+        self.completed = True
+        db.session.commit()
+    
+    def addItem(self,item):
+        items = json.loads(self.items)
+        items.append(item)
+        self.items = json.dumps(items)
+        db.session.commit()
+
+    def getOrder(self):
+        return {"id":self.id,"resturant_id":self.resturant_id,"user_id":self.user_id,"items":json.loads(self.items)}
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+    
+
+
+
+
+
